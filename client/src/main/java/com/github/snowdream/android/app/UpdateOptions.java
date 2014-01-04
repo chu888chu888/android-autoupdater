becf50399b98167d3cafc992c29e165a84f1c0ba
snowdream <yanghui1986527@gmail.com>
2014-01-04 23:42:23 +0800

    [Modified] change the type of the UpdatePeriod form enum to class

--- client/src/main/java/com/github/snowdream/android/app/UpdateOptions.java
+++ client/src/main/java/com/github/snowdream/android/app/UpdateOptions.java
@@ -60,35 +60,9 @@ public final class UpdateOptions {
         }
 
         long period = 0;
-        switch (updatePeriod) {
-            case NEVER:
-                period = -1;
-                break;
-            case EACH_ONE_DAY:
-                period = 1 * 24 * 60 * 60 * 1000;
-                break;
-            case EACH_TWO_DAYS:
-                period = 2 * 24 * 60 * 60 * 1000;
-                break;
-            case EACH_THREE_DAYS:
-                period = 3 * 24 * 60 * 60 * 1000;
-                break;
-            case EACH_ONE_WEEK:
-                period = 7 * 24 * 60 * 60 * 1000;
-                break;
-            case EACH_TWO_WEEKS:
-                period = 14 * 24 * 60 * 60 * 1000;
-                break;
-            case EACH_THREE_WEEKS:
-                period = 21 * 24 * 60 * 60 * 1000;
-                break;
-            case EACH_ONE_MONTH:
-                period = 30 * 24 * 60 * 60 * 1000;
-                break;
-            case EACH_TIME:
-            default:
-                period = 0;
-                break;
+
+        if (updatePeriod != null){
+            period = updatePeriod.getPeriodMillis();
         }
 
         if (period == 0) { //each time
@@ -157,7 +131,7 @@ public final class UpdateOptions {
     }
 
     public static class Builder {
-        private UpdatePeriod updatePeriod = UpdatePeriod.EACH_TIME;
+        private UpdatePeriod updatePeriod = new UpdatePeriod(UpdatePeriod.EACH_TIME);
         private boolean forceUpdate = false;
         private boolean autoUpdate = false;
         private String checkUrl = null;
--- client/src/main/java/com/github/snowdream/android/app/UpdatePeriod.java
+++ client/src/main/java/com/github/snowdream/android/app/UpdatePeriod.java
@@ -2,52 +2,96 @@ package com.github.snowdream.android.app;
 
 /**
  * The Period wich is used to check for update.<br/>
- *
+ * <p/>
  * Created by snowdream on 12/30/13.
  */
-public enum UpdatePeriod {
-    /**
-     * Never check for Update
-     */
-    NEVER,
-
+public class UpdatePeriod {
     /**
      * Each time you check for update.see@UpdateManager#check
      */
-    EACH_TIME,
+    public static final int EACH_TIME = 0;
 
     /**
      * Check for Update Every Day
      */
-    EACH_ONE_DAY,
+    public static final int EACH_ONE_DAY = 1;
 
     /**
      * Check for Update Every Two Days
      */
-    EACH_TWO_DAYS,
+    public static final int EACH_TWO_DAYS = 2;
 
     /**
      * Check for Update Every Three Days
      */
-    EACH_THREE_DAYS,
+    public static final int EACH_THREE_DAYS = 3;
 
     /**
      * Check for Update Every Week.
      */
-    EACH_ONE_WEEK,
+    public static final int EACH_ONE_WEEK = 7;
 
     /**
      * Check for Update Two Weeks.
      */
-    EACH_TWO_WEEKS,
+    public static final int EACH_TWO_WEEKS = 14;
 
     /**
      * Check for Update Three Weeks.
      */
-    EACH_THREE_WEEKS,
+    public static final int EACH_THREE_WEEKS = 21;
 
     /**
      * Check for Update Every Month.
      */
-    EACH_ONE_MONTH,
+    public static final int EACH_ONE_MONTH = 30;
+
+    /**
+     * The Minimum Period
+     */
+    public static final int EACH_MIN = 0;
+
+    /**
+     * The Maximum Period
+     */
+    public static final int EACH_MAX = 365;
+
+    private int period = -1;
+
+    public UpdatePeriod(int period) {
+        if (period >= EACH_MIN && period <= EACH_MAX){
+            this.period = period;
+        }else if (period < EACH_MIN){
+            this.period = EACH_MIN;
+        }else {
+            this.period = EACH_MAX;
+        }
+    }
+
+    /**
+     * Get the period
+     * the unit is Day.
+     * @return
+     */
+    public int getPeriod() {
+        return period;
+    }
+
+    /**
+     * Set the period
+     * the unit is Day.
+     * @param period
+     */
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    /**
+     * Get the period with the unit microsecond.
+     *
+     * @return
+     */
+    public long getPeriodMillis(){
+        return this.period  * 24 * 60 * 60 * 1000;
+    }
 }
